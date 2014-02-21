74f580a98b45b8e807c7889a6ab48a833598289c
Bing Lu <magpie727@gmail.com>
2014-02-20 22:07:13 -0500

    Insertion Sort List

---
+++ InsertionSortList.java
@@ -0,0 +1,41 @@
+/*
+ * Key point: handle the head of list, fakeHead strategy
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+            
+        ListNode fakeHead = new ListNode(Integer.MIN_VALUE);
+        ListNode current = head;
+        
+        while(current != null){
+            ListNode next = current.next;
+            ListNode prev = fakeHead;
+            
+            //find out the inserting position
+            while(prev.next != null && prev.next.val <current.val)
+                prev = prev.next;
+                
+            //insert into the sorted part    
+            current.next = prev.next;
+            prev.next = current;
+            
+            current = next;
+        }
+        
+        return fakeHead.next;
+    }
+}
